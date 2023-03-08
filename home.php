8e43060ef6e7d40033a4d6bb7d6bab007737267f
sahan-madusha <sahanmadusha001@gmail.com>
2023-03-08 19:53:35 +0530

    menu bar

--- home.php
+++ home.php
@@ -1,137 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IIT Student LMS - Login Page</title>
 
-    <link rel="stylesheet" href="style.css">
-    
+    <link rel="stylesheet" href="./style.css">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
 </head>
+
 <body>
-    <div>
-        <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <!-- Container wrapper -->
-            <div class="container-fluid">
-                <!-- Toggle button -->
-                <button
-                class="navbar-toggler"
-                type="button"
-                data-mdb-toggle="collapse"
-                data-mdb-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                >
-                <i class="fas fa-bars"></i>
-                </button>
 
-                <!-- Collapsible wrapper -->
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Navbar brand -->
-                <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                    <img
-                    src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                    height="15"
-                    alt="MDB Logo"
-                    loading="lazy"
-                    />
-                </a>
-                <!-- Left links -->
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Team</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Projects</a>
-                    </li>
-                </ul>
-                <!-- Left links -->
-                </div>
-                <!-- Collapsible wrapper -->
+    <!-- navigation bar -->
+    <nav class="d-flex flex-lg-row flex-column align-items-baseline justify-content-between homeNavBar p-2">
 
-                <!-- Right elements -->
-                <div class="d-flex align-items-center">
-                <!-- Icon -->
-                <a class="text-reset me-3" href="#">
-                    <i class="fas fa-shopping-cart"></i>
-                </a>
+        <div class="d-flex flex-row align-items-baseline">
+            <h2 class="LoginPageHead mx-2"><i class="bi bi-mortarboard-fill"></i> IIT LMS</h2>
+            <h2 class="mx-3 menuBar d-block d-lg-none" onclick="navBar()"><i class="bi bi-list"></i></h2>
+        </div>
 
-                <!-- Notifications -->
-                <div class="dropdown">
-                    <a
-                    class="text-reset me-3 dropdown-toggle hidden-arrow"
-                    href="#"
-                    id="navbarDropdownMenuLink"
-                    role="button"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
-                    >
-                    <i class="fas fa-bell"></i>
-                    <span class="badge rounded-pill badge-notification bg-danger">1</span>
-                    </a>
-                    <ul
-                    class="dropdown-menu dropdown-menu-end"
-                    aria-labelledby="navbarDropdownMenuLink"
-                    >
-                    <li>
-                        <a class="dropdown-item" href="#">Some news</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">Another news</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </li>
-                    </ul>
+        <!---->
+        <div id="menubar1" class="d-lg-flex d-none flex-lg-row flex-column align-items-baseline justify-content-between">
+            <!-- left side -->
+            <button class="AttendenceBtn rounded-pill boader px-3 py-1 fw-bold my-2">Attendence</button>
+
+            <div class="d-flex flex-row justify-content-evenly mt-1 ms-3 resNavCompo">
+                <div class="dropdown mx-3">
+                    <button class="dropbtn p-0 my-2">Recent</button>
+                    <div class="dropdown-content">
+                        <a href="#">Recent 1</a>
+                        <a href="#">Recent 2</a>
+                        <a href="#">Recent 3</a>
+                    </div>
                 </div>
-                <!-- Avatar -->
-                <div class="dropdown">
-                    <a
-                    class="dropdown-toggle d-flex align-items-center hidden-arrow"
-                    href="#"
-                    id="navbarDropdownMenuAvatar"
-                    role="button"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
-                    >
-                    <img
-                        src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                        class="rounded-circle"
-                        height="25"
-                        alt="Black and White Portrait of a Man"
-                        loading="lazy"
-                    />
-                    </a>
-                    <ul
-                    class="dropdown-menu dropdown-menu-end"
-                    aria-labelledby="navbarDropdownMenuAvatar"
-                    >
-                    <li>
-                        <a class="dropdown-item" href="#">My profile</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">Settings</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">Logout</a>
-                    </li>
-                    </ul>
+                <div class="dropdown mx-3">
+                    <button class="dropbtn p-0 my-2">Courses</button>
+                    <div class="dropdown-content">
+                        <a href="#">Courses 1</a>
+                        <a href="#">Courses 2</a>
+                        <a href="#">Courses 3</a>
+                    </div>
                 </div>
-                </div>
-                <!-- Right elements -->
             </div>
-            <!-- Container wrapper -->
-            </nav>
-        <!-- Navbar -->
-    </div>
+
+            <!-- left side -->
+            <div class="d-flex flex-column flex-lg-row align-items-baseline me-3">
+                <div class="mx-3 mt-1 resNavCompo ">
+                    <i class="bi bi-search"></i>
+                    <input type="text" class="rounded-pill border border-0 homeSearchBtn">
+                </div>
+                <div class="d-flex flex-row align-items-baseline resNavCompo">
+                    <i class="bi bi-bell-fill"></i>
+                    <h6 class="mx-2">P.D.G.S.M samarasinghe</h6>
+                    <img class="rounded-circle my-auto mx-auto" width="30px" height="30px" src="./src/img/userimg.png" alt="user image">
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+<script src="./script.js"></script>
 </body>
+
 </html>
